2b2eafc93b64910eeff154c86646eff643a9f15e
Simon-Cheek <simon.cheek@yahoo.com>
2025-09-19 09:22:54 -0400

    Query set search

--- skTaskApp/views.py
+++ skTaskApp/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.request import Request
+
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -7,3 +10,15 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        query_set = Task.objects.all()
+        request: Request = self.request # Ignore IDE TypeError
+        status = request.query_params.get("status")
+
+        # Filter on Status Directly
+        if status:
+            if status not in Task.Status.values:
+                raise ValidationError("Invalid Query Param Status")
+            query_set = query_set.filter(status=status)
+        return query_set
